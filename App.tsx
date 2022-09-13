c778259be7dcdc28c5c0073aed3483d73a9d7280
David Stiennon <david.stiennon@gmail.com>
2022-09-13 08:31:09 -0500

    start of app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    console.log('dude')
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Text style={styles.header}>This is seriously awesome!</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        fontSize: 50,
+        textAlign: 'center'
+    }
 });
---
+++ SoundData.ts
@@ -0,0 +1,36 @@
+type Sound = {
+    name: string;
+    image: string;
+    file: string | null;
+}
+
+
+const sounds: Sound[] = [
+    {
+        name: 'Sad Trombone',
+        image: 'trombone.png',
+        file: null
+    },
+    {
+        name: 'Ode to Joy',
+        image: 'trombone.png',
+        file: null
+    },
+    {
+        name: 'Ode to Joy',
+        image: 'trombone.png',
+        file: null
+    },
+    {
+        name: 'Ode to Joy',
+        image: 'trombone.png',
+        file: null
+    },
+    {
+        name: 'Ode to Joy',
+        image: 'trombone.png',
+        file: null
+    }
+]
+
+export default sounds
